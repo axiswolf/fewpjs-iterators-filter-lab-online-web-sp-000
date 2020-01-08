273bd2b531b1026d9a17eaf44efcd3d9852a07d5
Karen V <karenvo@usc.edu>
2020-01-08 10:13:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution here
 
 function findMatching(drivers, name) {
-    return drivers.filter(x => x == name)
+    return drivers.filter(x => x = name)
 }
 
 function fuzzyMatch() {
