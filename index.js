21b5564e08bff18ebf7340c58ad43a16093c36b3
Karen V <karenvo@usc.edu>
2020-01-08 10:29:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ function findMatching(drivers, name) {
     return drivers.filter(x => x == name)
 }
 
-function fuzzyMatch() {
+function fuzzyMatch(drivers, letter) {
+    for (let i of drivers) {
+      
+    }
+}
 
+function matchName(drivers, input) {
+    const x = drivers.filter(input)
+    if (x) return x
 }
